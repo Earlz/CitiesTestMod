2f6bde5baa05afec738f338f691b9fcd448dfcf6
Jordan Earls <earlz@earlz.net>
2015-03-21 01:18:59 -0400

    Make it actually adjust something

--- ModMain.cs
+++ ModMain.cs
@@ -24,15 +24,32 @@ namespace TestMod
     }
     public class InjectionPoint: LoadingExtensionBase
     {
+        static string locker = "";
         public override void OnLevelLoaded(LoadMode mode)
         {
             ChirpLogger.ChirpLog.Debug("hmmm something");
 
             ChirpLogger.ChirpLog.Info("loaded");
-            InceptorInterceptor.Add("x", new InceptorInterceptor.Interceptor((@this, name, parms) =>
+            InceptorInterceptor.Add("System.Void IndustrialBuildingAI::GetPollutionRates(System.Int32,System.Int32&,System.Int32&)", new InceptorInterceptor.Interceptor((@this, name, parms) =>
             {
-                File.AppendAllText("C:\\dev\\debug.log", "uh huh");
-                return null;
+                //note: chirplogger is apparently broken when using from different threads or in some way can not handle this and will silently fail to log anything
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("--begin--");
+                int i=0;
+                foreach (var p in parms)
+                {
+                    sb.AppendFormat("parms[{0}] = {1}\r\n", i, p == null ? "null" : p.ToString());
+                    i+=1;
+                }
+                sb.AppendLine("--end--");
+                lock(locker)
+                {
+                    File.AppendAllText("C:\\dev\\debug.txt", sb.ToString());
+                }
+                //groundpollution is 1
+                parms[1] = 0;
+                parms[2] = 10; //noise pollution
+                return new object(); //return an object to tell the intercepted method to return immediately after this (return null to tell it to continue through with the body of the method
             }));
 
             base.OnLevelLoaded(mode);
